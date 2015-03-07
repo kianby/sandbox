190e5093a799137aee260c7fbfb3a0ecb01b9944
Yax <kianby@madyanne.fr>
2015-03-07 21:00:57 +0100

    Draft cosysnode JS on Web site side.

--- cosysnode/routes/comments.js
+++ cosysnode/routes/comments.js
@@ -9,12 +9,26 @@ router.all('/', function (req, res, next) {
 })
 
 router.get('/', function(req, res, next) {
-  res.json([
-    {"nickname":"Yax", "comment":"What's up?"},
-    {"nickname":"Bob", "comment":"Silly question"},
-    {"nickname":"Yax", "comment":"Nop"} 
-  ]);
+  res.json(
+    {
+      "comments": [
+      {
+        "author": "Bob",
+        "site": null,
+        "date": "2014-12-21T08:36:38.000Z",
+        "gravatar": null,
+        "preview": "Du classique mais de la **qualité** !\n\nBonne continuation",
+      },
+      {
+        "author": "Yax",
+        "site": "http://www.blogduyax.madyanne.fr",
+        "date": "2014-12-21T08:36:38.000Z",
+        "gravatar": "308a3596152a79231f3feedc49afa4ef",
+        "preview": "Merci ! J'envisage de développer mon propre gestionnaire de …",
+      }
+      ]
+    }
+  );
 });
-
 
 module.exports = router;
